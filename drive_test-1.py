675d51ac734841937a2b54ad350186beabddd56f
John Biddiscombe <biddisco@cscs.ch>
2014-11-30 22:24:28 +0100

    Fix light sensor read and add handling of no brick detected

--- drive_test-1.py
+++ drive_test-1.py
@@ -4,6 +4,8 @@ import time
 import threading
 import random
 import sys, select, os
+import matplotlib
+#matplotlib.use('Agg')
 import pylab
 from pylab import *
 import nxt
@@ -21,29 +23,43 @@ soundport = nxt.PORT_2
 # size of plot array to keep as we generate new data
 num_plotted = 100
 num_retained = 1000 # unused at moment
-samples_per_s = 10
+samples_per_s = 5
 
 # mutex for sensor thread and graph access to data
 lock = threading.Lock()
 
 # detect Lego brick
-brick = nxt.locator.find_one_brick(debug=False)
-print brick
-
+try:
+  brick = nxt.locator.find_one_brick(debug=False)
+  print brick
+except:
+  brick = None
+  print "Lego brick not found"
+  
+  
 # ultrasonic sensor
-#u = nxt.sensor.Ultrasonic(brick, usndport)
+if brick is not None:
+  u = nxt.sensor.Ultrasonic(brick, usndport)
+  print u
 
 # Touch button
-#t = nxt.sensor.Touch(brick, touchport)
+  t = nxt.sensor.Touch(brick, touchport)
+  print t
 
 # Sound sensor
-s = nxt.sensor.Sound(brick, soundport)
-print s
+  s = nxt.sensor.Sound(brick, soundport)
+  print s
 
 # Light sensor
-l = nxt.sensor.Light(brick, lightport)
-print l
-
+  l = nxt.sensor.Light(brick, lightport)
+  l.set_illuminated(True)
+  print l
+else:
+  u = None
+  t = None
+  s = None
+  l = None
+  
 # global index for latest timevalue
 data_index = -1
 
@@ -68,57 +84,49 @@ t_values=[]
 s_values=[]
 l_values=[]
 
-# This just simulates reading from a sensor
-def data_listener():
-    global u_values, t_values, data_index, lock, samples_per_s
-    while True:
-        v = 0
-        for i in range(0,5):
-            v += random()
-        v = v/5.0
-        lock.acquire()
-        u_values.append(v)
-        data_index += 1
-        lock.release()
-        time.sleep(1.0/samples_per_s)
-
 def get_sensor_data():
     global u, s, t, l, l_values, u_values, s_values, t_values, data_index, lock, samples_per_s
-    while True:
-#        for i in range(0,5):
-#            val = u.get_sample()
-#        touch = t.is_pressed()
+    while True: 
+      if brick is not None:
+        for i in range(0,5):
+            val = u.get_sample()
+        touch = t.is_pressed()
         snd = s.get_sample()*100.0/255.0
-        lght = l.get_sample()
-        # lock and write data
-        lock.acquire()
-#        u_values.append(val)
-        s_values.append(snd)
-        l_values.append(lght)
-        print lght
-#        t_values.append(60 if touch else 40)
-        data_index += 1
-        lock.release()
-        print snd
-#val, touch
-        time.sleep(1.0/samples_per_s)
+        lght = l.get_lightness()
+      else:
+        val = 50 + random()*5
+        touch = 0
+        snd = 10 + random()*5
+        lght = 30 + random()*10
+
+      # lock and write data
+      lock.acquire()
+      u_values.append(val)
+      s_values.append(snd)
+      l_values.append(lght)
+      t_values.append(60 if touch else 40)
+      data_index += 1
+      lock.release()
+      print "\nsound ", snd, "\ndist ", val, "\nbutton ", touch, "\nlight ", lght
+      time.sleep(1.0/samples_per_s)
 
 def RealtimePlotter():
     global l_values, u_values, s_values, t_values, data_index, lock, samples_per_s
     if (data_index<0):
         return
-    lock.acquire()
-    min_index = max(data_index + 1 - num_plotted, 0)
-    max_index = max(num_plotted, data_index + 1)
-    xdata = pylab.arange(min_index, data_index + 1, 1)
-#    line_u[0].set_data(xdata, pylab.array(u_values[-num_plotted:]))
-#    line_t[0].set_data(xdata, pylab.array(t_values[-num_plotted:]))
-    line_s[0].set_data(xdata, pylab.array(s_values[-num_plotted:]))
-    line_l[0].set_data(xdata, pylab.array(l_values[-num_plotted:]))
-    lock.release()
-#    ax.axis([min_index, max_index, -1.5, 1.5])
-    ax.axis([min_index, max_index, 0, 100])
-    manager.canvas.draw()
+    while True:
+      lock.acquire()
+      min_index = max(data_index + 1 - num_plotted, 0)
+      max_index = max(num_plotted, data_index + 1)
+      xdata = pylab.arange(min_index, data_index + 1, 1)
+      line_u[0].set_data(xdata, pylab.array(u_values[-num_plotted:]))
+      line_t[0].set_data(xdata, pylab.array(t_values[-num_plotted:]))
+      line_s[0].set_data(xdata, pylab.array(s_values[-num_plotted:]))
+      line_l[0].set_data(xdata, pylab.array(l_values[-num_plotted:]))
+      lock.release()
+      ax.axis([min_index, max_index, 0, 100])
+      manager.canvas.draw()
+      time.sleep(1.0/samples_per_s)
 
 def forward(b, power, time):
     left  = Motor(b, lmotport) 
@@ -135,27 +143,33 @@ def reverse(b, power, time):
 # disable blocking
 pylab.ion()
 pylab.show(block=False)
-
+#RealtimePlotter()
+#manager.canvas.draw()
 
 if __name__ == '__main__':
-    thread1 = threading.Thread(target=get_sensor_data)
-    thread1.daemon = True
-    thread1.start()
+  # start a thread which will sample all the lego sensors
+  thread1 = threading.Thread(target=get_sensor_data)
+  thread1.daemon = True
+  thread1.start()
+# start a thread which will continuously plot lege sensor data
+  thread2 = threading.Thread(target=RealtimePlotter)
+  thread2.daemon = True
+  thread2.start()
 
-    state = 0
-    try:
-        while True:
-            RealtimePlotter()
+  state = 0
+  try:
+      while True:
+#            RealtimePlotter()
+          if brick is not None:
             if (s.get_sample()*100.0/255.0)>99:
                 if (state==0):
-                    forward(brick,100,100)
+#                    forward(brick,60,10000)
                     state = 1
-                    time.sleep(1)
+                    time.sleep(0.5)
                 elif (state==1):
-                    reverse(brick,100,100)
+#                    reverse(brick,100,10)
                     state = 0
-                    time.sleep(1)
+                    time.sleep(0.5)
 
-    except KeyboardInterrupt:
-        pass
-
+  except KeyboardInterrupt:
+      pass
